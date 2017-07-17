a9df776584f1e1ca5d4f5bc2089eee44007c7008
ljp <ljp3326@naver.com>
2017-07-17 02:59:58 -0700

    tcp error fix

--- test.c
+++ test.c
@@ -38,23 +38,6 @@
 		struct sniff_tcp {
 			u_short th_sport;	/* source port */
 			u_short th_dport;	/* destination port */
-			tcp_seq th_seq;		/* sequence number */
-			tcp_seq th_ack;		/* acknowledgement number */
-			u_char th_offx2;	/* data offset, rsvd */
-	#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
-			u_char th_flags;
-	#define TH_FIN 0x01
-	#define TH_SYN 0x02
-	#define TH_RST 0x04
-	#define TH_PUSH 0x08
-	#define TH_ACK 0x10
-	#define TH_URG 0x20
-	#define TH_ECE 0x40
-	#define TH_CWR 0x80
-	#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-			u_short th_win;		/* window */
-			u_short th_sum;		/* checksum */
-			u_short th_urp;		/* urgent pointer */
 			};
 
 	 int main(int argc, char *argv[])
@@ -102,18 +85,20 @@
 			return(2);
 		}
 		int i=0;
-		/* Grab a packet */
+
 		while(1){
 		i = pcap_next_ex(handle, &header, &packet);
 		ethernet = (struct sniff_ethernet*)(packet);
 		ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
 		size_ip = IP_HL(ip)*4;
 		size_tcp = TH_OFF(tcp)*4;
-		payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);	
+	
 		char src_ip[1024];
 		char dst_ip[1024];
 		strcpy(src_ip,inet_ntoa(ip->ip_src));
 		strcpy(dst_ip,inet_ntoa(ip->ip_dst));
+
+		payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
 		//printf("*test1\n");
 		
 		//packet += 14;
@@ -131,6 +116,11 @@
 		//}
 		//printf("*test4\n");
 
+		void tcp_port(const u_char *packet)
+		{
+		printf("Tcp Source Port = %d \n", ((packet[34]*256) + packet[35]));
+		printf("Tcp Destination Port = %d \n", ((packet[36]*256) + packet[37]));
+		}
 		
 		printf("eth.dmac : ");
 		for(int d = 0; d<6; d++){
@@ -151,6 +141,7 @@
 		printf("Destination ip : %s",dst_ip);
 		printf("\n");
 		
+		tcp_port(packet);		
 
 		printf("Data : %s", payload);
 		printf("\n");
