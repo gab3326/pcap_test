3bef52647c3ecb4d3b8f11269bdcc06e547be06e
ljp <ljp3326@naver.com>
2017-07-17 03:20:24 -0700

    bug fix end

--- test.c
+++ test.c
@@ -12,14 +12,25 @@
 #define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
 #define IP_HL(ip)	(((ip)->ip_vhl) & 0x0f)
 #define IP_V(ip)	(((ip)->ip_vhl) >> 4)
+#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
+#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
+#define TH_FIN 0x01
+#define TH_SYN 0x02
+#define TH_RST 0x04
+#define TH_PUSH 0x08
+#define TH_ACK 0x10
+#define TH_URG 0x20
+#define TH_ECE 0x40
+#define TH_CWR 0x80
+#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
 		/* Ethernet header */
 		struct sniff_ethernet {
 		u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
 		u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
-		
-		u_short ether_type; /* IP? ARP? RARP? etc */
+		u_short ether_type; /* IP? ARP? RARP? etc */		
 		};
 
+		/* IP header */
 		struct sniff_ip {
 		u_char ip_vhl;		/* version << 4 | header length >> 2 */
 		u_char ip_tos;		/* type of service */
@@ -34,56 +45,64 @@
 		
 		/* TCP header */
 		typedef u_int tcp_seq;
-
 		struct sniff_tcp {
-			u_short th_sport;	/* source port */
-			u_short th_dport;	/* destination port */
-			};
+		u_short th_sport;	/* source port */
+		u_short th_dport;	/* destination port */
+		tcp_seq th_seq;		/* sequence number */
+		tcp_seq th_ack;		/* acknowledgement number */
+		u_char th_offx2;	/* data offset, rsvd */
+		u_char th_flags;
+		u_short th_win;		/* window */
+		u_short th_sum;		/* checksum */
+		u_short th_urp;		/* urgent pointer */
+		};
 
-	 int main(int argc, char *argv[])
-	 {
-		pcap_t *handle;			/* Session handle */
-		char *dev;			/* The device to sniff on */
-		char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-		struct bpf_program fp;		/* The compiled filter */
-		char filter_exp[] = "port 80";	/* The filter expression */
-		bpf_u_int32 mask;		/* Our netmask */
-		bpf_u_int32 net;		/* Our IP */
-		struct pcap_pkthdr header;	/* The header that pcap gives us */
-		const u_char *packet;		/* The actual packet */
-		const struct sniff_ethernet *ethernet;
-		const struct sniff_ip *ip; /* The IP header */
-		u_int size_ip;
-		u_int size_tcp;
-		const struct sniff_tcp *tcp;
-		const char *payload;
-		/* Define the device */
-		dev = pcap_lookupdev(errbuf);
-		if (dev == NULL) {
-			fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-			return(2);
-		}
-		/* Find the properties for the device */
-		if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-			fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
-			net = 0;
-			mask = 0;
-		}
-		/* Open the session in promiscuous mode */
-		handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-		if (handle == NULL) {
-			fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-			return(2);
-		}
-		/* Compile and apply the filter */
-		if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-			fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-			return(2);
-		}
-		if (pcap_setfilter(handle, &fp) == -1) {
-			fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-			return(2);
-		}
+int main(int argc, char *argv[])
+{
+	pcap_t *handle;			/* Session handle */
+	char *dev;			/* The device to sniff on */
+	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
+	struct bpf_program fp;		/* The compiled filter */		
+	char filter_exp[] = "port 80";	/* The filter expression */
+	bpf_u_int32 mask;		/* Our netmask */
+	bpf_u_int32 net;		/* Our IP */
+	struct pcap_pkthdr header;	/* The header that pcap gives us */
+	const u_char *packet;		/* The actual packet */
+	const struct sniff_ethernet *ethernet;
+	const struct sniff_ip *ip; /* The IP header */
+	u_int size_ip;
+	u_int size_tcp;
+	const struct sniff_tcp *tcp;
+	const char *payload;
+
+	/* Define the device */
+	dev = pcap_lookupdev(errbuf);
+	if (dev == NULL) {
+		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+		return(2);
+	}
+	/* Find the properties for the device */
+	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+		net = 0;
+		mask = 0;
+	}
+	/* Open the session in promiscuous mode */
+	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if (handle == NULL) {
+		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+		return(2);
+	}
+	/* Compile and apply the filter */
+	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+	if (pcap_setfilter(handle, &fp) == -1) {
+		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+
 		int i=0;
 
 		while(1){
@@ -99,21 +118,14 @@
 		strcpy(dst_ip,inet_ntoa(ip->ip_dst));
 
 		payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
-		//printf("*test1\n");
+		
+		//printf("*test1\n"); <-error test area
 		
 		//packet += 14;
 		//printf("*test2\n");
-		//
 		
-		
-		//size_tcp = TH_OFF(tcp)*4;
 		//printf("*test3\n");
 		
-		
-		//if (size_ip < 20) {
-		//printf("   * Invalid IP header length: %u bytes\n", size_ip);
-		//return;
-		//}
 		//printf("*test4\n");
 
 		void tcp_port(const u_char *packet)
@@ -122,14 +134,14 @@
 		printf("Tcp Destination Port = %d \n", ((packet[36]*256) + packet[37]));
 		}
 		
-		printf("eth.dmac : ");
+		printf("Eth.dmac : ");
 		for(int d = 0; d<6; d++){
 			printf("%02x",ethernet->ether_dhost[d]);
 			printf(" : ");
 			}
 		printf("\n");
 		
-		printf("eth.smac : ");
+		printf("Eth.smac : ");
 		for(int s = 0; s<6; s++){
 			printf("%02x",ethernet->ether_shost[s]);
 			printf(" : ");
@@ -141,15 +153,15 @@
 		printf("Destination ip : %s",dst_ip);
 		printf("\n");
 		
-		tcp_port(packet);		
+		tcp_port(packet); 		
 
-		printf("Data : %s", payload);
+		printf("Data : \n %s", payload);
 		printf("\n");
-		//printf("tcp : %d",ntohs(tcp->th_sport));
+		printf("\n");
+
+		//printf("tcp : %d",ntohs(tcp->th_sport));  <-error why???!!!
 		}
 		
-
-		/* And close the session */
 		pcap_close(handle);
 		return(0);
 	 }
