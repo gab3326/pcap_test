af36bc0797afdb3e3b984cdb9b5a59479816ac8e
ljp <ljp3326@naver.com>
2017-07-24 08:01:33 -0700

    data start location

--- test.c
+++ test.c
@@ -172,13 +172,13 @@ int main(int argc, char *argv[])
 			printf("\tTcp Destination port : %d\n",ntohs(tcp->th_dport));
 		}
 		
-		if(size_data>53){
+		//if(size_data>=54)
 		printf("\tData_size : %d\n", size_data);
 		printf("\tData : \n %s", data);
 		printf("\n");
 		printf("\n");
 		}
-		}
+		
 		
 		pcap_close(handle);
 		return(0);
